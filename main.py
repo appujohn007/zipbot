9b069c9b6c8864896f9614408bbd78b4893efcff
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-07 20:34:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ token = os.environ.get('BOT_TOKEN', "6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3
 
 app = Client("zipBot", app_id, app_key, bot_token=token)
 
+# Import the handlers
+import bot.private
 
 if __name__ == '__main__':
-
     try:
         mkdir("static")  # create static files folder
     except FileExistsError:
