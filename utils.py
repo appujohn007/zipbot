4c39263cf816c83f10ceda4bc0793c5e70816f0b
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-07 20:44:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,40 +5,32 @@ from os import listdir
 # ========= DB build =========
 db = Database()
 
-
 class User(db.Entity):
-    uid = PrimaryKey(int, auto=True)
+    uid = PrimaryKey(int, size=64)  # Allows larger values for uid
     status = Required(int)  # status-user: "INSERT"/"NOT-INSERT"
-
 
 db.bind(provider='sqlite', filename='zipbot.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
-
 
 # ========= helping func =========
 def dir_work(uid: int) -> str:
     """ static-user folder """
     return f"static/{uid}/"
 
-
 def zip_work(uid: int) -> str:
     """ zip-archive file """
     return f'static/{uid}.zip'
-
 
 def list_dir(uid: int) -> list:
     """ items in static-user folder """
     return listdir(dir_work(uid))
 
-
 def up_progress(current, total, msg: Message):
     """ edit status-msg with progress of the uploading """
     msg.edit(f"**Upload progress: {current * 100 / total:.1f}%**")
 
-
 # ========= MSG class =========
 class Msg:
-
     def start(msg: Message) -> str:
         """ return start-message text """
         txt = f"Hey {msg.from_user.mention}!\n" \
