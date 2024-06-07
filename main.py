7a7a89aa916e5138beb88f69b3e0b5ece9b629e1
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-07 18:45:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import mkdir
 
 app_id = int(os.environ.get("API_ID", 10471716))
 app_key = os.environ.get('API_HASH', "f8a1b21a13af154596e2ff5bed164860")
-token = os.environ.get('BOT_TOKEN', "6999401413:AAHgF1ZpUsCT5MgWX1Wky7GbegyeHvzi2AU")
+token = os.environ.get('BOT_TOKEN', "6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3yX0")
 
 app = Client("zipBot", app_id, app_key, bot_token=token)
 
