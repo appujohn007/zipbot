ce7af19c372b4a78239611f9a05dc7f866f734f9
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-09 19:13:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,6 +12,21 @@ from pyrogram.errors import FloodWait
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+
+db = Database()
+
+class User(db.Entity):
+    uid = PrimaryKey(int, size=64)  # Allows larger values for uid
+    status = Required(int)  # status-user: "INSERT"/"NOT-INSERT"
+
+db.bind(provider='sqlite', filename='zipbot.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
+
+
+
+
+
 def dir_work(uid):
     """Return the directory path for user files."""
     return f"static/{uid}/"
@@ -107,3 +122,25 @@ async def zip_and_send(uid, zip_path, msg, stsmsg):
     except Exception as e:
         logger.error(f"Error in zip_and_send: {e}")
         msg.reply(f"An error occurred while creating or sending the zip file: {e}")
+
+
+
+# ========= MSG class =========
+class Msg:
+    def start(msg: Message) -> str:
+        """ return start-message text """
+        txt = f"Hey {msg.from_user.mention}!\n" \
+              "\nI can compress files into an archive." \
+              "\nJust send /zip, and follow the instructions."
+        return txt
+
+    zip = "Send the files you want to compress, and at the end send /done after all the files have been downloaded.\n" \
+          "\n\nNote: due to upload limit, the total size of the file(s) can be at most 2GB."
+    too_big = "Note: due to upload limit, the total size of the file(s) can be at most 2GB."
+    too_much = "Note: the total number of files can be at most 500."
+    send_zip = "Send /zip to compress the files."
+    zipping = "Start compressing {} files..."
+    uploading = "Uploading archive..."
+    unknow_error = "An unknown error occurred."
+    downloading = "Downloading..."
+    zero_files = "No files were sent."
