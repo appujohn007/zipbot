bfe8945f467b99c09948ab8f33bb405f71bfaa31
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-11 14:52:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,14 +57,28 @@ def start(client, msg: types.Message):
 
         
         help_text = """
-Hello Iam a Advanced Telegram bot to make zip files
-How to use me?
-Send /help to see 🙂
+WHAT I CAN DO
+
+*I Can make Zips
+* Supports various medias Like Video, Images, Documents, pdf, apk and many more
+
+HOW TO USE ME
+
+* Send /zip command along with your name for the zip without .zip extension
+eg: `/zip iodevs`
+*After That Send The files which you want to include in the zip
+* Then Patiently wait for the downloading to be completed
+* Then Send /done to start making zipa with the files
+
+======================
+
+Join [.io devs](https://t.me/botio_devs) to support me
 """
-        msg.reply(help_text)
+        msg.reply(help_text, parse_mode=enums.ParseMode.MARKDOWN)
     except Exception as e:
         logger.error(f"Error in start: {e}")
         msg.reply("An error occurred. Please try again later.") 
+        
 
 
 
