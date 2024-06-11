9468cf616fc396fda4ca735bd905fe49f7c7ed23
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-11 15:11:32 +0530

    Create scripts.py

---
+++ scripts.py
@@ -0,0 +1,30 @@
+start_text = """
+Hello Iam a Advanced Telegram bot to make zip files
+How to use me?
+Send /help to see 🙂
+"""
+
+help_text = """
+WHAT I CAN DO
+
+*I Can make Zips
+* Supports various medias Like Video, Images, Documents, pdf, apk and many more
+
+HOW TO USE ME
+
+* Send /zip command along with your name for the zip without .zip extension
+eg: `/zip iodevs`
+*After That Send The files which you want to include in the zip
+* Then Patiently wait for the downloading to be completed
+* Then Send /done to start making zipa with the files
+
+======================
+
+Join [.io devs](https://t.me/botio_devs) to support me
+"""
+
+
+invalid_cmd = """
+Sorry,
+You want to send like /zip {yourfilename}
+eg: /zip summerphoto
